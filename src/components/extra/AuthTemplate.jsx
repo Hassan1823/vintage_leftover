304e6e14e1b4eca1a9dc6364c9c1c7d4505616e6
Hassan Zaib <hassan.zaib223@gmail.com>
2024-12-21 21:05:50 +0500

    saving progress

--- src/components/extra/AuthTemplate.jsx
+++ src/components/extra/AuthTemplate.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { FiFacebook, FiInstagram } from "react-icons/fi";
-import { phoneNumber } from "../../lib/data";
 import { Link } from "react-router-dom";
+import { phoneNumber } from "../../lib/data";
 
 const AuthTemplate = ({ children }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   return (
     <div className="flex flex-col duration-500 gap-2 items-center justify-center w-full h-[100vh]">
       {/* main login container */}
       <div className="w-[90%] duration-500 md:w-[70%] lg:w-[50%] text-xs h-auto border gap-5 shadow-md flex flex-col rounded-md py-[8vh] justify-center items-center">
-        {/* <Link to={"/"}> */}
-        <img
-          src="/logo.png"
-          alt="logo"
-          className="duration-500 w-auto max-md:h-[8vh] h-[12vh] object-contain"
-        />
-        {/* </Link> */}
+        <Link to={"/"}>
+          <img
+            src="/logo.png"
+            alt="logo"
+            className="duration-500 w-auto max-md:h-[8vh] h-[12vh] object-contain"
+          />
+        </Link>
 
         {children}
       </div>
