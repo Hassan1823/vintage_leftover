aa72e8f0f58e9e3d7479687d699257ce77963377
Hassan Zaib <hassan.zaib223@gmail.com>
2025-01-06 19:22:39 +0500

    payout

--- src/pages/Checkout/ConfirmOrder.jsx
+++ src/pages/Checkout/ConfirmOrder.jsx
@@ -321,7 +321,7 @@ const ConfirmOrder = () => {
           />
 
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
+            <div className="flex items-center justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
               <DialogPanel
                 transition
                 className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
